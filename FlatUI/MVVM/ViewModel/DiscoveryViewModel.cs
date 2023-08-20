2e6ff66f41bdd86abdc11e5a4ea1f18dc0b8674a
Akarsh711 <paliwalap7@gmail.com>
2023-08-20 23:56:05 +0530

    completed

--- FlatUI/MVVM/ViewModel/DiscoveryViewModel.cs
+++ FlatUI/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using FlatUI.Core;
 
 namespace FlatUI.MVVM.ViewModel
 {
-    internal class HomeViewModel
+    internal class DiscoveryViewModel
     {
-        
     }
-
 }
--- FlatUI/MVVM/ViewModel/MainViewModel.cs
+++ FlatUI/MVVM/ViewModel/MainViewModel.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlatUI.Core;
 
 namespace FlatUI.MVVM.ViewModel
 {
-    //internal class MainViewModel
-    //{
-    //    public HomeViewModel HomeVm { get; set; }
-    //    private object _currentView;
-    //    public object MyProperty
-    //    {
-    //        get { return _currentView; }
-    //        set 
-    //        {
-    //            _currentView = value;
-    //            OnPropertyChanged();
-    //        }
-    //    }
-    //    public MainViewModel()
-    //    {
-    //        HomeVm = new HomeViewModel();
+    class MainViewModel: ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+        private object _currentView;
 
-    //    }
-    //}
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
    
+
 }
